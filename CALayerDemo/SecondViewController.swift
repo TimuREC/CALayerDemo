a6ffcd273fc9609dc43ee3e96b4ccf64306f5d4b
Timur Begishev <rpsi-mono@yandex.ru>
2021-01-30 00:14:42 +0300

    Learning of cornerRadius, maskToBounds, border and shadow setup

--- CALayerDemo/SecondViewController.swift
+++ CALayerDemo/SecondViewController.swift
@@ -9,8 +9,22 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-	@IBOutlet weak var imageView: UIImageView!
-	@IBOutlet weak var button: UIButton!
+	@IBOutlet weak var imageView: UIImageView! {
+		didSet {
+			imageView.layer.cornerRadius = imageView.frame.size.height / 2
+			imageView.layer.masksToBounds = true
+			imageView.layer.borderColor = UIColor.lightGray.cgColor
+			imageView.layer.borderWidth = 10
+		}
+	}
+	@IBOutlet weak var button: UIButton! {
+		didSet {
+			button.layer.shadowOffset = CGSize(width: 0, height: 10)
+			button.layer.shadowOpacity = 0.7
+			button.layer.shadowRadius = 10
+			button.layer.shadowColor = UIColor.red.cgColor
+		}
+	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
