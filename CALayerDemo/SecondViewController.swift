fc4c58bd05b40920a79f3957d7e196020ac6ad59
Timur Begishev <rpsi-mono@yandex.ru>
2021-01-29 23:57:43 +0300

    Project prepared for work

---
+++ CALayerDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CALayerDemo
+//
+//  Created by Timur Begishev on 29.01.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var button: UIButton!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
